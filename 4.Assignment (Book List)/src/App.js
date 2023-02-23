cc69b8236855578d422d514981896f056ebcf738
Mohammad Tareq <mohammad.tareq@apsissolutions.com>
2023-02-23 17:22:29 +0600

    st up for ass 4

---
+++ 4.Assignment (Book List)/src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Cart from './components/cart/Cart';
+import Navbar from './components/Navbar';
+import ProductWrapper from './components/product/ProductWrapper';
+
+const App = () => {
+	return (
+		<>
+			<Navbar />
+			<main className="py-16">
+				<Routes>
+					<Route path="/cart" element={<Cart />} />
+					<Route path="/" element={<ProductWrapper />} />
+					<Route path="*" element={<>Not Found</>} />
+				</Routes>
+			</main>
+		</>
+	);
+};
+
+export default App;
