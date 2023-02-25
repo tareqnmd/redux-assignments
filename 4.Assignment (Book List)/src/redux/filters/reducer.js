7f5e3113a73a73cef042afdf8f7d606ea94cee6e
tareqnmd <tareqnmd@gmail.com>
2023-02-25 23:56:52 +0600

    filter up

--- 4.Assignment (Book List)/src/redux/filters/reducer.js
+++ 4.Assignment (Book List)/src/redux/filters/reducer.js
@@ -1,8 +1,13 @@
+import { FILTER_SEARCH, FILTER_TYPE } from './actionTypes';
 import initialState from './initialState';
 
 const reducer = (state = initialState, action) => {
 	const { type, payload } = action;
 	switch (type) {
+		case FILTER_SEARCH:
+			return { ...state, searchText: payload };
+		case FILTER_TYPE:
+			return { ...state, type: payload };
 		default:
 			return state;
 	}
