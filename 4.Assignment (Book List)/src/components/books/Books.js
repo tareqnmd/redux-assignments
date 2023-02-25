61d965039e083960b32698bfa7a46b37e8a31f16
tareqnmd <tareqnmd@gmail.com>
2023-02-26 00:11:43 +0600

    up

--- 4.Assignment (Book List)/src/components/books/Books.js
+++ 4.Assignment (Book List)/src/components/books/Books.js
@@ -1,16 +1,35 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { filterType } from '../../redux/filters/actions';
 import Book from './Book';
 
 const Books = () => {
+	const dispatch = useDispatch();
+	const { type } = useSelector((state) => state.filter);
+	const filterChange = (type) => {
+		dispatch(filterType(type));
+	};
 	return (
 		<div className="order-2 xl:-order-1">
 			<div className="flex items-center justify-between mb-12">
 				<h4 className="mt-2 text-xl font-bold">Book List</h4>
 
 				<div className="flex items-center space-x-4">
-					<button className="filter-btn active-filter" id="lws-filterAll">
+					<button
+						className={`filter-btn ${type === 'All' && 'active-filter'}`}
+						id="lws-filterAll"
+						onClick={() => {
+							filterChange('All');
+						}}
+					>
 						All
 					</button>
-					<button className="filter-btn" id="lws-filterFeatured">
+					<button
+						className={`filter-btn ${type === 'Featured' && 'active-filter'}`}
+						id="lws-filterFeatured"
+						onClick={() => {
+							filterChange('Featured');
+						}}
+					>
 						Featured
 					</button>
 				</div>
