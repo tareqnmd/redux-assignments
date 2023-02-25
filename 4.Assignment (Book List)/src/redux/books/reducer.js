736377960a4b49c7610594858e9f97dab9214776
tareqnmd <tareqnmd@gmail.com>
2023-02-26 00:03:01 +0600

    up

--- 4.Assignment (Book List)/src/redux/books/reducer.js
+++ 4.Assignment (Book List)/src/redux/books/reducer.js
@@ -1,8 +1,30 @@
+import { BOOK_ADD, BOOK_DELETE, BOOK_EDIT } from './actionTypes';
 import initialState from './initialState';
+
+const nextTodoId = (data) => {
+	const maxId = data.reduce((maxId, todo) => Math.max(todo.id, maxId), 0);
+	return maxId + 1;
+};
 
 const reducer = (state = initialState, action) => {
 	const { type, payload } = action;
+
 	switch (type) {
+		case BOOK_ADD:
+			const newBook = {
+				id: nextTodoId(state),
+				...payload,
+			};
+			return [...state, newBook];
+		case BOOK_EDIT:
+			return state.map((item) => {
+				if (item.id === payload.id) {
+					return { ...payload };
+				}
+				return item;
+			});
+		case BOOK_DELETE:
+			return state.filter((book) => book.id !== payload);
 		default:
 			return state;
 	}
