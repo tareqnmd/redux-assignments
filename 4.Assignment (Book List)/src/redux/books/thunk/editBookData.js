4945bb28621570fbe11c92779f499ab5d6b77ec4
tareqnmd <tareqnmd@gmail.com>
2023-02-28 08:01:05 +0600

    up

--- 4.Assignment (Book List)/src/redux/books/thunk/editBookData.js
+++ 4.Assignment (Book List)/src/redux/books/thunk/editBookData.js
@@ -4,7 +4,7 @@ const editBookData = (data) => {
 	return async (dispatch) => {
 		const response = await fetch(`http://localhost:9000/books/${data.id}`, {
 			method: 'PATCH',
-			body: JSON.stringify({ data }),
+			body: JSON.stringify(data),
 			headers: { 'Content-Type': 'application/json;charset=UTF-8' },
 		});
 		const book = await response.json();
