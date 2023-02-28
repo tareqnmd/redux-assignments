aee262bd5f4b9a82972a7ce20879eaaa87a21506
tareqnmd <tareqnmd@gmail.com>
2023-02-28 08:00:18 +0600

    aad

--- 4.Assignment (Book List)/src/redux/books/thunk/addBookData.js
+++ 4.Assignment (Book List)/src/redux/books/thunk/addBookData.js
@@ -4,7 +4,7 @@ const addBookData = (data) => {
 	return async (dispatch) => {
 		const response = await fetch(`http://localhost:9000/books`, {
 			method: 'POST',
-			body: JSON.stringify({ data }),
+			body: JSON.stringify(data),
 			headers: { 'Content-Type': 'application/json;charset=UTF-8' },
 		});
 		const book = await response.json();
