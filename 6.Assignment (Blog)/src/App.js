44de9f02694c2d4fc39f7301f092266b99ca9df3
tareqnmd <tareqnmd@gmail.com>
2023-03-09 00:28:38 +0600

    up

--- 6.Assignment (Blog)/src/App.js
+++ 6.Assignment (Blog)/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Blog from './pages/Blog';
+import Home from './pages/Home';
 
 function App() {
-	return <Router></Router>;
+	return (
+		<Router>
+			<Navbar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/blog/:blogId" element={<Blog />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
