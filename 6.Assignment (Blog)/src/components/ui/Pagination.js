f39215cc850cb7d845f665f39533b9e525322574
tareqnmd <tareqnmd@gmail.com>
2023-03-10 12:31:55 +0600

    up

--- 6.Assignment (Blog)/src/components/ui/Pagination.js
+++ 6.Assignment (Blog)/src/components/ui/Pagination.js
@@ -1,7 +1,4 @@
-import { useSelector } from 'react-redux';
-
 const Pagination = () => {
-	const { page } = useSelector((state) => state.blogs);
 	return (
 		<section className="flex gap-2 items-center justify-end">
 			<div className="bg-blue-600 text-white px-4 py-1 rounded-full">Prev</div>
