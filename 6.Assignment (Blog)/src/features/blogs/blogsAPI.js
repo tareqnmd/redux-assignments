500083ea385d787370940dbb0f01ccd4f579911d
tareqnmd <tareqnmd@gmail.com>
2023-03-10 10:36:37 +0600

    sort updated

--- 6.Assignment (Blog)/src/features/blogs/blogsAPI.js
+++ 6.Assignment (Blog)/src/features/blogs/blogsAPI.js
@@ -1,8 +1,22 @@
 import axiosInstance from '../../utils/axios';
 
 const getBlogs = async (sortType, filterType) => {
-	const query = `?${filterType === 'saved' ? 'isSaved=true' : ''}`;
-	const response = await axiosInstance.get(`/blogs/${query}`);
+	const queryFilter = `${filterType === 'saved' ? 'isSaved=true' : ''}`;
+	const querySort = `${
+		sortType === 'most_liked'
+			? '_sort=likes&_order=desc'
+			: sortType === 'newest'
+			? '_sort=id&_order=desc'
+			: ''
+	}`;
+	const query = `?${
+		queryFilter.length
+			? `${queryFilter}${querySort.length ? `&${querySort}` : ''}`
+			: querySort.length
+			? `${querySort}`
+			: ''
+	}`;
+	const response = await axiosInstance.get(`/blogs${query}`);
 	return response.data;
 };
 export default getBlogs;
