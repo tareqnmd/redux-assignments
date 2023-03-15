59a8ed514dde2a1b8dd28b0996d1ab0799538ddc
tareqnmd <tareqnmd@gmail.com>
2023-03-16 01:46:44 +0600

    add

--- 7.Assignment (Job Finder)/src/components/Sidebar.js
+++ 7.Assignment (Job Finder)/src/components/Sidebar.js
@@ -1,59 +1,49 @@
+import { Link } from 'react-router-dom';
+
 const Sidebar = () => {
 	return (
 		<div className="sidebar">
 			<nav>
 				<ul className="space-y-4">
 					<li>
-						<a
-							href="/jobs"
+						<Link
+							to="/"
 							className="main-menu menu-active"
 							id="lws-alljobs-menu"
 						>
 							<i className="fa-solid fa-briefcase"></i>
 							<span> All Available Jobs</span>
-						</a>
+						</Link>
 						<ul className="space-y-6 lg:space-y-2 ">
 							<li>
-								<a
-									className="sub-menu"
-									href="/jobs/internship"
-									id="lws-internship-menu"
-								>
-									<i className="fa-solid fa-stop !text-[#FF5757]"></i>
+								<span className="sub-menu">
+									<i className="mr-1 fa-solid fa-stop !text-[#FF5757]"></i>
 									Internship
-								</a>
+								</span>
 							</li>
 							<li>
-								<a
-									className="sub-menu"
-									href="/jobs/fulltime"
-									id="lws-fulltime-menu"
-								>
-									<i className="fa-solid fa-stop !text-[#FF8A00]"></i>
+								<span className="sub-menu">
+									<i className="mr-1 fa-solid fa-stop !text-[#FF8A00]"></i>
 									Full Time
-								</a>
+								</span>
 							</li>
 							<li>
-								<a
-									className="sub-menu"
-									href="/jobs/remote"
-									id="lws-remote-menu"
-								>
-									<i className="fa-solid fa-stop !text-[#56E5C4]"></i>
+								<span className="sub-menu">
+									<i className="mr-1 fa-solid fa-stop !text-[#56E5C4]"></i>
 									Remote
-								</a>
+								</span>
 							</li>
 						</ul>
 					</li>
 					<li>
-						<a
-							href="/jobs"
+						<Link
+							to="/create"
 							className="main-menu"
 							id="lws-addJob-menu"
 						>
-							<i className="fa-solid fa-file-circle-plus"></i>
+							<i className="mr-1 fa-solid fa-file-circle-plus"></i>
 							<span>Add NewJob</span>
-						</a>
+						</Link>
 					</li>
 				</ul>
 			</nav>
