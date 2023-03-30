1a0713780f689b88ce672c4c02eb5bd3ca0817b9
tareqnmd <tareqnmd@gmail.com>
2023-03-30 14:04:11 +0600

    team done

--- 9-10.Assignment (Task Manager)/src/components/sidebar/Teams.js
+++ 9-10.Assignment (Task Manager)/src/components/sidebar/Teams.js
@@ -1,12 +1,27 @@
+import { useGetTeamQuery } from '../../features/team/teamAPi';
 import Team from './Team';
 
 const Teams = () => {
+	const { data, isLoading, isError, error } = useGetTeamQuery();
+	let content = null;
+	if (isLoading) {
+		content = 'Loading..';
+	} else if (!isLoading && isError) {
+		content = error?.data ?? 'Error Found';
+	} else if (!isLoading && !isError && data?.length === 0) {
+		content = 'No Data Found';
+	} else if (!isLoading && !isError && data?.length > 0) {
+		content = data?.map((team) => (
+			<Team
+				key={team?.id}
+				team={team}
+			/>
+		));
+	}
 	return (
 		<div className="mt-8">
 			<h3 className="text-xl font-bold">Team Members</h3>
-			<div className="mt-3 space-y-4">
-				<Team />
-			</div>
+			<div className="mt-3 space-y-4">{content}</div>
 		</div>
 	);
 };
---
+++ 9-10.Assignment (Task Manager)/src/features/team/teamSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const teamSlice = createSlice({
+	name: 'team',
+	initialState,
+	reducers: {},
+});
+
+export default teamSlice.reducer;
